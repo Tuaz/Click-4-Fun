7bd1a9d6705e73eac149f3ebcf9cd034698601bd
Tuaz <mathuas_r@live.dk>
2016-11-12 23:46:42 +0100

    Getting Started

---
+++ Click 4 Fun/Assets/Scripts/BasePlayer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlayer {
+
+    private string playerName;
+    private string playerLvl;
+    private BaseClass playerClass;
+
+    private int strength;
+    private int stamina;
+
+    private int armor;
+    private int speed;
+    private int hitpoints;
+
+    public int Strength
+    {
+        get { return strength; }
+        set { strength = value; }
+    }
+    public int Stamina
+    {
+        get { return stamina; }
+        set { stamina = value; }
+    }
+    public int Armor
+    {
+        get { return armor; }
+        set { armor = value; }
+    }
+    public int Speed
+    {
+        get { return speed; }
+        set { speed = value; }
+    }
+    public int Hitpoints
+    {
+        get { return hitpoints; }
+        set { hitpoints = value; }
+    }
+}
---
+++ Click 4 Fun/Assets/Scripts/BaseWarrior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseWarrior : BaseClass {
+
+    public BaseWarrior()
+    {
+        Strength = 10;
+        Stamina = 10;
+        Hitpoints = 100;
+        Armor = 0;
+        Speed = 1;
+    }
+	
+}
